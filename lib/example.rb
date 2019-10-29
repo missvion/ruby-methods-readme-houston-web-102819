0321c754f5a2da3fe84f2fb136e3e1f642e993c5
Vivian Tran <viviantran94@gmail.com>
2019-10-29 11:39:58 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -13,4 +13,18 @@ puts phrase
 puts phrase
 
 def hello_world_ten_times
+  phrase = "Hello World!"
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+end
+
+
   
